397c41c4eb3efc440e8ca8525fd64b41c84abbc9
Rupak <rupaklamapythondeveloper@gmail.com>
2020-08-11 15:31:12 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('react hooks');
+  const [error, setError] = useState(null);
+
+  // to display loading message
+  const [loadingMessage, setLoadingMessage] = useState(false);
+
+  // to access DOM element
+  // taking ref object & assigned it to one of the element 
+  const searchInputRef = useRef();
+
+  // fetching data from api
+  const fetchResults = async () => {
+    // set to true before loading which displays a message
+    setLoadingMessage(true)
+
+    // error handling 
+    try {
+      const { data } = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`)
+      console.log(data.hits)
+      setResults(data.hits)
+    } catch (error) {
+      // display error piece of state
+      setError(error)
+    }
+    
+    
+    // set to false after loading 
+    setLoadingMessage(false)
+  }
+
+  useEffect(() => {
+    fetchResults();
+    // eslint-disable-next-line 
+  }, []);
+
+  // submit handler
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchResults();
+    // setQuery('');
+  }
+
+  const handleClearSearch = () => {
+    setQuery('')
+    searchInputRef.current.focus()
+  }
+
+  const renderResults = results.map(result => {
+
+    const { objectID, url, title } = result;
+
+    return (
+      <li key={objectID}>
+        <a href={url}>{title}</a>
+      </li>
+    )
+  })
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        value={query} 
+        onChange={e => setQuery(e.target.value)} 
+        ref={searchInputRef}
+        />
+
+        <button type="button" onClick={fetchResults} >Search</button>
+        <button type="button" onClick={handleClearSearch}>Clear</button>
+      </form>
+      
+      <ul>
+        { loadingMessage ? <div>Loading results...</div> : renderResults }
+      </ul>
+
+      {error && <div>{error.message}</div>}
+    </div>// displaying error message
+
+  ) // on true, set loading message
 }
+// ref={searchInputRef} - doing this will make us access to the input attribs
 
 export default App;
